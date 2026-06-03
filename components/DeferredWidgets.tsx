d95fcbb9f977887afd890f0329a3a9b550d1af41
haithum <h.baselabdelfa@gmail.com>
2026-06-03 10:24:17 -0500

    feat(cta): replace SupportBubble chatbot with HomeEquityFloatingCTA widget and add styling

--- components/DeferredWidgets.tsx
+++ components/DeferredWidgets.tsx
@@ -1,27 +1,41 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { usePathname } from 'next/navigation';
+import { HomeEquityFloatingCTA } from '@/components/HomeEquityFloatingCTA';
 
 const GoogleAnalytics = dynamic(() => import('@/components/GoogleAnalytics'), { ssr: false });
 const FacebookPixel = dynamic(() => import('@/components/FacebookPixel'), { ssr: false });
 const MobileStickyCallButton = dynamic(() => import('@/components/MobileStickyCallButton'), { ssr: false });
-const SupportBubble = dynamic(() => import('@/components/SupportBubble'), { ssr: false });
 const ExitIntentModal = dynamic(() => import('@/components/ExitIntentModal'), { ssr: false });
 const ConditionalSiteEnhancements = dynamic(() => import('@/components/ConditionalSiteEnhancements'), { ssr: false });
 const AccessibilityWidget = dynamic(() => import('@/components/AccessibilityWidget'), { ssr: false });
 const CookieBanner = dynamic(() => import('@/components/CookieBanner'), { ssr: false });
 
 export default function DeferredWidgets() {
+  const pathname = usePathname();
+  const isWholesaleLanding = pathname === '/';
+
   return (
     <>
       <GoogleAnalytics />
       <FacebookPixel />
-      <ConditionalSiteEnhancements />
-      <ExitIntentModal />
-      <SupportBubble />
-      <MobileStickyCallButton />
-      <AccessibilityWidget />
-      <CookieBanner />
+      {isWholesaleLanding ? (
+        <>
+          <ExitIntentModal />
+          <HomeEquityFloatingCTA />
+          <AccessibilityWidget />
+        </>
+      ) : (
+        <>
+          <ConditionalSiteEnhancements />
+          <ExitIntentModal />
+          <HomeEquityFloatingCTA />
+          <MobileStickyCallButton />
+          <AccessibilityWidget />
+          <CookieBanner />
+        </>
+      )}
     </>
   );
 }
